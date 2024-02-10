7cf8340a0e4ff49e8bdd4c4732fd0b0b05d03184
Thomas Hynes <tjhynes8@icloud.com>
2024-02-10 14:43:38 -0500

    Added dummy login form

--- pagesImpl/login/loginImpl.tsx
+++ pagesImpl/login/loginImpl.tsx
@@ -1,14 +1,60 @@
 import styled from '@emotion/styled'
+import React, { useState } from 'react';
 import { PageImplView } from '@pagesImpl/__components__/PageImplView'
 
-export default function MainImpl() {
+export default function LoginImpl() {
   return (
     <PageImplView>
-      <LoginBackgroundWrapper>Login Page</LoginBackgroundWrapper>
+      <LoginBackgroundWrapper>
+        <h1>Login Page</h1>
+        <LoginForm></LoginForm>
+        </LoginBackgroundWrapper>
     </PageImplView>
   )
 }
 
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = ()  => {
+    // You can handle form submission logic here, e.g., sending data to an API
+    console.log('Submitted:', { username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username or Email:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+
 const LoginBackgroundWrapper = styled.div`
   background: #f5f7fa;
+  background-size: auto;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
 `
