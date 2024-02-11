c0ff801aedca2ce597584876f5844a2b7330e0f0
Wonho <biocle8339@gmail.com>
2024-02-10 23:21:14 -0500

    ADD Basic Header

---
+++ pagesImpl/__components__/BookMarkIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const BookMarkIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M5 21V5C5 4.45 5.196 3.97933 5.588 3.588C5.98 3.19667 6.45067 3.00067 7 3H17C17.55 3 18.021 3.196 18.413 3.588C18.805 3.98 19.0007 4.45067 19 5V21L12 18L5 21Z"
+        fill="#B1B1B1"
+      />
+    </svg>
+  )
+}
--- pagesImpl/main/mainImpl.tsx
+++ pagesImpl/main/mainImpl.tsx
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled'
+import { Header } from '@pagesImpl/__components__/Header'
 import { PageImplView } from '@pagesImpl/__components__/PageImplView'
 
 export default function MainImpl() {
   return (
     <PageImplView>
+      <Header />
       <MainBackgroundWrapper>Main Page</MainBackgroundWrapper>
     </PageImplView>
   )
