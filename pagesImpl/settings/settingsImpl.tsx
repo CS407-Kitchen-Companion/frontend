faff8ef4bde7b040e20902e60381fe8a6e1a4232
Thomas Hynes <tjhynes8@icloud.com>
2024-03-24 17:18:53 -0400

    editpost and edit profile

--- pagesImpl/settings/settingsImpl.tsx
+++ pagesImpl/settings/settingsImpl.tsx
@@ -2,77 +2,80 @@ import styled from '@emotion/styled'
 import { useRouter } from"next/navigation";
 import { FormEvent } from 'react'
 import React, { useState } from 'react';
+import { Header } from '@pagesImpl/__components__/Header'
 import { PageImplView } from '@pagesImpl/__components__/PageImplView'
 import { useDispatch, useSelector } from 'react-redux'
 import { userDataAction, selectPassword } from '@lib/store/userData/userData.slice'
 
-export default function ForgotImpl() {
+export default function SettingsImpl() {
+  const router = useRouter()
+  const changePwd = () => {
+    router.push('/settings/changePassword')
+  }
+  const editProfile = () => {
+    router.push('/settings/editProfile')
+  }
   return (
     <PageImplView>
-      <LoginBackgroundWrapper>
-        <h1>Change Password</h1>
-        <ForgotForm></ForgotForm>
-        </LoginBackgroundWrapper>
+      <Header/>
+      <BackgroundWrapper>
+        
+      <ContentWrapper>
+      <h1 style={
+        {
+          padding: '2%',
+        }
+      }>Settings</h1>
+      <ButtonWrapper>
+   <StyledButton onClick={changePwd}>
+    Change Password
+   </StyledButton>
+   </ButtonWrapper>
+   <ButtonWrapper>
+    <StyledButton onClick={editProfile}>
+    Edit Profile
+    </StyledButton>
+    </ButtonWrapper>
+    </ContentWrapper>
+        </BackgroundWrapper>
     </PageImplView>
   )
 }
 
 
-const ForgotForm = () => {
-  const [oldpwd, setOldPwd] = useState('');
-  const [password, setPass] = useState('');
-  const[newPwd, setNewPwd]= useState('');
-  const [id, setId] = useState('30');
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-  const handleSubmit =  async (event: FormEvent<HTMLFormElement>)  => {
-    event.preventDefault();
-      console.log(id)
-      setPass(newPwd)
-      dispatch(userDataAction.setOldPwd({oldpwd}))
-      dispatch(userDataAction.setNewPwd({newPwd}))
-      dispatch(userDataAction.setPassword({password}))
-      dispatch(userDataAction.setId({id}))
-      dispatch(userDataAction.requestFlowUpdatePassword())
-
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="oldPwd">Old Password:</label>
-        <input
-          type="text"
-          id="oldPwd"
-          value={oldpwd}
-          onChange={(e) => setOldPwd(e.target.value)}
-          required
-        />
-        <div></div>
-        <label htmlFor="newPwd">New Password:</label>
-        <input
-          type="text"
-          id="newPwd"
-          value={newPwd}
-          onChange={(e) => setNewPwd(e.target.value)}
-          required
-        />
-      </div>
-      
-      
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
 
 
-const LoginBackgroundWrapper = styled.div`
+const BackgroundWrapper = styled.div`
+  margin: 0;
+  padding: 0;
   background: #f5f7fa;
   background-size: auto;
   width: 100vw;
   height: 100vh;
   text-align: center;
+  
+`
+const ContentWrapper = styled.div`
+  
+background: white;
+background-size: auto;
+height: 60vh;
+width: 40%;
+margin-top: 2%;
+margin-left: 30%;
+  
+`
+const StyledButton = styled.button`
+color: lightgray;
+font-weight: bold;
+background: #2D3566;
+width: 250px;
+height: 50px;
+border-radius:  30px ;
+border: 1px solid #2D3566;
+`
+const ButtonWrapper = styled.div`
+  margin-top: 5%;
 `
 /*
 <div>
